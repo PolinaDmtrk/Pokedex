720778e6ae998aaeefe79cb42cd051cec1fd4b4a
Polina Dmitrenko <polinadmitrenko99@mail.ru>
2019-04-10 21:38:09 +0500

    Updates

---
+++ precache-manifest.a207ea4fc3a453e119d56517eb0abe59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  },
+  {
+    "revision": "37ac994383f6c6f0ad8f",
+    "url": "/Pokedex/static/js/main.4a9b6d60.chunk.js"
+  },
+  {
+    "revision": "f2f3c7cf53d564e1644c",
+    "url": "/Pokedex/static/js/2.8597fce3.chunk.js"
+  },
+  {
+    "revision": "37ac994383f6c6f0ad8f",
+    "url": "/Pokedex/static/css/main.30a091c3.chunk.css"
+  },
+  {
+    "revision": "4cd59df5d266e1034c6a0dbf04f17710",
+    "url": "/Pokedex/index.html"
+  }
+];
